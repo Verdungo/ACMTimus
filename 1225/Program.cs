c38d793a6b4c0d74a409a5c5f9c0e4765ba99cfe
Verdungo <myxomop.k66@gmail.com>
2015-11-06 14:30:33 +0500

    1225

---
+++ 1225/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace _1225
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int stripes = int.Parse(Console.ReadLine());
+            uint[] variants = new uint[stripes+1];
+
+            if (stripes == 1)
+            {
+                Console.WriteLine(2);
+            }
+            else
+            {
+                variants[1] = 2;
+                variants[2] = 2;
+                uint answer = 2;
+
+                for (int i = 3; i < stripes+1; i++)
+                {
+                    variants[i] = variants[i - 1] + variants[i - 2];
+                    answer = variants[i];
+                }
+                Console.WriteLine(answer);
+            }
+        }
+    }
+}
