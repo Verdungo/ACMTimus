1b448fae14e12ccfca2687b55b2dda2524af84e2
Verdungo <myxomop.k66@gmail.com>
2015-11-06 09:05:06 +0500

    1319

---
+++ 1319/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace _1319
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int size = int.Parse(Console.ReadLine());
+                                                                                                            // верхняя строка
+                                                                                                            // !!!!!!!!!!!!!!!!!!!!!!!!!!
+                                                                                                            // int a = (int)(i * (i - 1) / 2) + 1;
+            for (int s = 0; s < size; s++)
+            {
+                for (int i = size; i > 0; i--)
+                {
+                    int a = ((i + s) * ((i + s) - 1) / 2) + 1 + s;
+                    int d = (i+s > size) ? s+i-size : 0;
+                    Console.Write(a-d*d + " ");
+                }
+                Console.WriteLine();
+            }
+            Console.ReadLine();
+        }
+    }
+}
