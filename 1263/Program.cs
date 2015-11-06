e57077e8ecbb8dec42349730fb1d2123ef6f6809
Verdungo <myxomop.k66@gmail.com>
2015-11-06 10:11:52 +0500

    1263

---
+++ 1263/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1263
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] str = Console.ReadLine().Split(' ');
+            int condidates = int.Parse(str[0]);
+            int voters = int.Parse(str[1]);
+            int[] count = new int[condidates];
+
+            for (int i = 0; i < voters; i++)
+            {
+                int vote = int.Parse(Console.ReadLine());
+
+                count[vote-1]++;
+            }
+
+            for (int i = 0; i < condidates; i++)
+            {
+                var percentRes = 100.0 * (double)count[i] / voters;
+                Console.WriteLine(string.Format("{0:0.00}%",percentRes));
+            }
+        }
+    }
+}
