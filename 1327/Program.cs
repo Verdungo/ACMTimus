23f13c8161dfee597f10a4443eacfbf602935633
Verdungo <myxomop.k66@gmail.com>
2015-11-08 13:06:28 +0500

    1327

---
+++ 1327/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1327
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int firstDay = int.Parse(Console.ReadLine());
+            int lastDay = int.Parse(Console.ReadLine());
+
+            int fuzes = 0;
+            for (int day = firstDay; day <= lastDay; day++)
+            {
+                if (day%2 == 1) fuzes++;
+            }
+            Console.WriteLine(fuzes);
+
+        }
+    }
+}
